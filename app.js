89056155f23c6a76826b1f9ae4b59487fb56db47
Lancerio <comiarln@gmail.com>
2022-02-01 00:08:58 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+let scrollPosition = 0;
+let scrollAmount = 320;
+let maxScroll = 800;
+
+const scroll = document.querySelector('.scroll');
+const projectList = document.querySelector('.project-container');
+const leftBtn = document.getElementById('left-btn');
+const rightBtn = document.getElementById('right-btn');
+
+// Event Listeners
+rightBtn.addEventListener('click', () => {
+    scrollHorizontal(-1);
+});
+
+leftBtn.addEventListener('click', () => {
+    scrollHorizontal(1);
+});
+
+// Function
+function scrollHorizontal(n) {
+    scrollPosition += (n * scrollAmount);
+
+    if (-scrollPosition >= maxScroll) {
+        scrollPosition = -maxScroll;
+    }
+
+    if (scrollPosition >= 0) {
+        scrollPosition = 0;
+    }
+
+    projectList.style.left = scrollPosition + 'px';
+
+}
